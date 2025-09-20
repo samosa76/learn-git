dba28ec2efc0cbd30e9738d8559d844dd8affe4a
Sam_76 <muhammadusamah76@gmail.com>
2025-09-20 14:17:56 +0800

    Question-3

---
+++ question-3.js
@@ -0,0 +1,13 @@
+/**
+ * 
+ * Buatlah sebuah logika untuk menampilkan jam buka toko secara otomatis
+ * 
+ * Jika nilai kurang dari 79 maka score adalah D
+ * Jika nilai 70-80 maka score adalah C
+ * Jika nilai 81-90 maka score adalah B
+ * Jika nilai 91-100 maka score adalah A
+ * 
+ * 
+ */
+
+const nilai = 80
